8102ea87ef8b0689a34b737923ce3279b3757789
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-10-04 23:05:16 +0530

    Update forLaundry.sh

--- BatchFiles/forLaundry.sh
+++ BatchFiles/forLaundry.sh
@@ -1,26 +1,19 @@
 git clone https://github.com/keshavsoft/CrudGenV1
 git clone -c core.longpaths=true https://github.com/keshavsoft/LaundryFrontEnd
 git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForUsers
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndMazerVite
+git clone -c core.longpaths=true https://github.com/keshavsoft/crudFrontEnd
 
-cd LaundryFrontEnd
+cd CrudGenV1
 npm i
-
-cd ../FrontEndForUsers
-npm i
-npm run Users
-
-cd ../CrudGenV1
-npm i
-
 echo PORT=7019> .env
 echo DataPk=336>> .env
-
 node KCode/ForBat/Backend.js
 node KCode/ForBat/Database.js
 npm run home
 
 cd ../LaundryFrontEnd
-
+npm i
 npm run WashtexBranch
 npm run BranchToday
 npm run TodayReports
@@ -35,26 +28,22 @@ npm run Today-QrCodes
 npm run Today-Orders
 npm run AdminToday-QrCodes
 npm run AdminToday-Orders
-
 cp -arf ./publicDir ../CrudGenV1/public/Laundry
 
-cd ../FRONTENDMAZERVITE
+cd ../FrontEndForUsers
+npm i
+npm run Users
+cp -arf ./publicDir/LoginUsers ../CrudGenV1/public/LoginUsers
 
+cd ../FrontEndMazerVite
+npm i
 npm run FromBranchVoucher
 npm run FromBranchQrCodes
 npm run FromEntryVoucher
 npm run FromEntryQrCodes
-
 cp -arf ./publicDir ../CrudGenV1/public/Factory
 
 cd ../crudFrontEnd
-
+npm i
 npm run Dashboard
-
 cp -arf ./publicDir ../CrudGenV1/public/crudFrontEnd
-
-cd ../FrontEndForUsers
-
-npm run Users
-
-cp -arf ./publicDir/LoginUsers ../CrudGenV1/public/LoginUsers
