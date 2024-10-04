37af5eb4d583b70218ce6e63a9af5f13a6a370b7
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-10-04 23:10:36 +0530

    Update forLaundry.bat

--- BatchFiles/forLaundry.bat
+++ BatchFiles/forLaundry.bat
@@ -3,26 +3,19 @@
 git clone https://github.com/keshavsoft/CrudGenV1
 git clone -c core.longpaths=true https://github.com/keshavsoft/LaundryFrontEnd
 git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForUsers
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndMazerVite
+git clone -c core.longpaths=true https://github.com/keshavsoft/crudFrontEnd
 
-cd LaundryFrontEnd
+cd CrudGenV1
 call npm i
-
-cd ..\FrontEndForUsers
-call npm i
-call npm run Users
-
-cd ..\CrudGenV1
-call npm i
-
 echo PORT=7019> .env
 echo DataPk=336>> .env
-
-call node KCode/ForBat/Backend.js
-call node KCode/ForBat/Database.js
+node KCode/ForBat/Backend.js
+node KCode/ForBat/Database.js
 call npm run home
 
 cd ..\LaundryFrontEnd
-
+call npm i
 call npm run WashtexBranch
 call npm run BranchToday
 call npm run TodayReports
@@ -37,26 +30,22 @@ call npm run Today-QrCodes
 call npm run Today-Orders
 call npm run AdminToday-QrCodes
 call npm run AdminToday-Orders
-
 xcopy .\publicDir ..\CrudGenV1\public\Laundry /h /i /c /k /e /r /y
 
-cd ..\FRONTENDMAZERVITE
+cd ..\FrontEndForUsers
+call npm i
+call npm run Users
+xcopy .\publicDir\LoginUsers ..\CrudGenV1\public\LoginUsers /h /i /c /k /e /r /y
 
+cd ..\FrontEndMazerVite
+call npm i
 call npm run FromBranchVoucher
 call npm run FromBranchQrCodes
 call npm run FromEntryVoucher
 call npm run FromEntryQrCodes
-
 xcopy .\publicDir ..\CrudGenV1\public\Factory /h /i /c /k /e /r /y
 
 cd ..\crudFrontEnd
-
+call npm i
 call npm run Dashboard
-
 xcopy .\publicDir ..\CrudGenV1\public\crudFrontEnd /h /i /c /k /e /r /y
-
-cd ..\FrontEndForUsers
-
-call npm run Users
-
-xcopy .\publicDir\LoginUsers ..\CrudGenV1\public\LoginUsers /h /i /c /k /e /r /y
