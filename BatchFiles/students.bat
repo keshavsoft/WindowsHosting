98ef8795f8064ac0405d6e3042dc06c413de8d2c
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-10-04 12:24:31 +0530

    Update students.bat

--- BatchFiles/students.bat
+++ BatchFiles/students.bat
@@ -6,6 +6,8 @@ git clone -c core.longpaths=true https://github.com/keshavsoft/crudFrontEnd
 cd crudFrontEnd
 call npm i
 call npm run TableShow
+call npm run ProtectedTableShow
+call npm run CreateStatic
 
 cd ..\CrudGenV1
 call npm i
@@ -20,3 +22,5 @@ call npm run home
 cd ..\crudFrontEnd
 
 xcopy .\publicDir\TableShow ..\CrudGenV1\public\TableShow /h /i /c /k /e /r /y
+xcopy .\publicDir\Protected\Create\Static ..\CrudGenV1\public\Protected\Create\Static /h /i /c /k /e /r /y
+xcopy .\publicDir\Protected\Table\Show ..\CrudGenV1\public\Protected\Table\Show /h /i /c /k /e /r /y
