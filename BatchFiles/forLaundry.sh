9ca748c4b1b7d3f8d275e055f5e9a5055ba03ffa
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-10-04 19:57:18 +0530

    Create forLaundry.sh

---
+++ BatchFiles/forLaundry.sh
@@ -0,0 +1,60 @@
+git clone https://github.com/keshavsoft/CrudGenV1
+git clone -c core.longpaths=true https://github.com/keshavsoft/LaundryFrontEnd
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForUsers
+
+cd LaundryFrontEnd
+npm i
+
+cd ../FrontEndForUsers
+npm i
+npm run Users
+
+cd ../CrudGenV1
+npm i
+
+echo PORT=7019> .env
+echo DataPk=336>> .env
+
+node KCode/ForBat/Backend.js
+node KCode/ForBat/Database.js
+npm run home
+
+cd ../LaundryFrontEnd
+
+npm run WashtexBranch
+npm run BranchToday
+npm run TodayReports
+npm run Masters
+npm run ToFactory
+npm run NewOrders
+npm run CustomersToOrders
+npm run Main
+npm run BranchAll
+npm run AllReports
+npm run Today-QrCodes
+npm run Today-Orders
+npm run AdminToday-QrCodes
+npm run AdminToday-Orders
+
+cp -arf ./publicDir ../CrudGenV1/public/Laundry
+
+cd ../FRONTENDMAZERVITE
+
+npm run FromBranchVoucher
+npm run FromBranchQrCodes
+npm run FromEntryVoucher
+npm run FromEntryQrCodes
+
+cp -arf ./publicDir ../CrudGenV1/public/Factory
+
+cd ../crudFrontEnd
+
+npm run Dashboard
+
+cp -arf ./publicDir ../CrudGenV1/public/crudFrontEnd
+
+cd ../FrontEndForUsers
+
+npm run Users
+
+cp -arf ./publicDir/LoginUsers ../CrudGenV1/public/LoginUsers
