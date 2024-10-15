b2a38c0181014b799b476626614e1786d4e90d4d
Nalipi Sridhar Reddy <108881367+sridharreddy7831@users.noreply.github.com>
2024-10-15 21:45:40 +0530

    Update studentsWithUi.sh

--- BatchFiles/studentsWithUi.sh
+++ BatchFiles/studentsWithUi.sh
@@ -4,3 +4,16 @@ git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForClient
 
 cd crudFrontEnd
 npm i
+
+cd ../FrontEndForClients
+npm i
+
+cd ../CrudGenV1
+call npm i
+
+echo PORT=7020> .env
+echo DataPk=327>> .env
+
+node KCode/ForBat/Backend.js
+node KCode/ForBat/Database.js
+npm run home
