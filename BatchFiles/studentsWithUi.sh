8d93fe4cefee2bf5a7f042526eb63883256cd9c1
Nalipi Sridhar Reddy <108881367+sridharreddy7831@users.noreply.github.com>
2024-10-15 21:35:22 +0530

    Update studentsWithUi.sh

--- BatchFiles/studentsWithUi.sh
+++ BatchFiles/studentsWithUi.sh
@@ -2,3 +2,5 @@ git clone https://github.com/keshavsoft/CrudGenV1
 git clone -c core.longpaths=true https://github.com/keshavsoft/crudFrontEnd
 git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForClients
 
+cd crudFrontEnd
+npm i
