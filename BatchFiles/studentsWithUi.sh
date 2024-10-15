8f62b78bb41192470c8d27a3117c582a5236eebe
Nalipi Sridhar Reddy <108881367+sridharreddy7831@users.noreply.github.com>
2024-10-15 21:30:22 +0530

    Create studentsWithUi.sh

---
+++ BatchFiles/studentsWithUi.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/keshavsoft/CrudGenV1
+git clone -c core.longpaths=true https://github.com/keshavsoft/crudFrontEnd
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForClients
+
