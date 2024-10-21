fecbdd65f7f16677912a063f07d442cc3ee2f7ee
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-10-21 15:54:09 +0530

    Update Pk902.bat

--- BatchFiles/Garments/Pk902.bat
+++ BatchFiles/Garments/Pk902.bat
@@ -1,1 +1,15 @@
-hai
+@echo off
+
+git clone https://github.com/keshavsoft/CrudGenV2
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForClients
+
+cd CrudGenV2
+call npm i
+echo PORT=7902> .env
+echo DataPk=902>> .env
+node KCode/ForBatV5/Backend.js
+node KCode/ForBatV5/Database.js
+call npm run home
+
+cd ..\FrontEndForClients
+xcopy .\publicStatic\Garments ..\CrudGenV2\public\Garments /h /i /c /k /e /r /y
