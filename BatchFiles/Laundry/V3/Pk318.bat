9ccb539ba43f3d830f352fecbcfc808dc5a8f46c
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-11-16 10:03:42 +0530

    Create Pk318.bat

---
+++ BatchFiles/Laundry/V3/Pk318.bat
@@ -0,0 +1,45 @@
+@echo off
+
+git clone https://github.com/keshavsoft/CrudGenV2
+git clone -c core.longpaths=true https://github.com/keshavsoft/LaundryFrontEndForAdmin
+git clone -c core.longpaths=true https://github.com/keshavsoft/LaundryFrontEndForBranch
+git clone -c core.longpaths=true https://github.com/keshavsoft/LaundryFrontEndForFactory
+
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForUsers
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndMazerVite
+git clone -c core.longpaths=true https://github.com/keshavsoft/crudFrontEnd
+
+cd CrudGenV2
+call npm i
+echo PORT=7318> .env
+echo DataPk=318>> .env
+node KCode/ForBatV5/Backend.js
+node KCode/ForBatV5/Database.js
+call npm run home
+
+cd ..\LaundryFrontEndForAdmin
+call npm i
+
+cd ..\LaundryFrontEndForBranch
+call npm i
+
+cd..\LaundryFrontEndForFactory
+call npm i
+
+cd ..\FrontEndForUsers
+call npm i
+
+cd ..\FrontEndMazerVite
+call npm i
+
+cd ..\crudFrontEnd
+call npm i
+
+cd ..\CrudGenV2
+call .\BatchFiles\ForLaundry\crudFrontEnd.bat
+call .\BatchFiles\ForLaundry\FrontEndForUsers.bat
+call .\BatchFiles\ForLaundry\FrontEndMazerVite.bat
+
+call .\BatchFiles\ForLaundry\LaundryFrontEndForAdmin.bat
+call .\BatchFiles\ForLaundry\LaundryFrontEndForBranch.bat
+call .\BatchFiles\ForLaundry\LaundryFrontEndForFactory.bat
