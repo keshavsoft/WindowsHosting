f1c1fadc2469c9424ccc5522e8eef8288a05cea8
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-12-16 16:07:42 +0530

    Create Pk327.bat

---
+++ BatchFiles/Students/V3/Pk327.bat
@@ -0,0 +1,26 @@
+
+@echo off
+
+git clone https://github.com/keshavsoft/CrudGenV3
+git clone -c core.longpaths=true https://github.com/keshavsoft/crudFrontEnd
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForClients
+
+cd CrudGenV3
+call npm i
+echo PORT=7327> .env
+echo DataPk=327>> .env
+node KCode/ForBatV5/Backend.js
+node KCode/ForBatV5/Database.js
+call npm run home
+
+cd ..\crudFrontEnd
+call npm i
+
+cd ..\FrontEndForClients
+call npm i
+
+cd ..\CrudGenV3
+call .\BatchFiles\ForStudents\ForUI.bat
+
+
+npm run Students
