72411e8c050ac5260d2ed83febbc38716b596ea6
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-03-22 18:49:55 +0530

    Update Pk328.bat

--- BatchFiles/WhatsApp/V2/Pk328.bat
+++ BatchFiles/WhatsApp/V2/Pk328.bat
@@ -16,6 +16,9 @@ echo '{"posts":[]}'> Data/inwards.json
 node KCode/ForBatV10/Backend.js
 node KCode/ForBatV10/Database.js
 call npm run home
+call npm run WA
+
+xcopy .\publicDir\WA .\public\WA /h /i /c /k /e /r /y
 
 cd ..\CrudFrontEndGulp
 call npm i
