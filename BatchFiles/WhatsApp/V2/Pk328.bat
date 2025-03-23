733493a7a04fdde48b819b112d5befb5baca357c
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-03-23 09:01:41 +0530

    Update Pk328.bat

--- BatchFiles/WhatsApp/V2/Pk328.bat
+++ BatchFiles/WhatsApp/V2/Pk328.bat
@@ -10,16 +10,6 @@ call npm i
 echo PORT=7328> .env
 echo DataPk=328>> .env
 
-mkdir Data
-echo {"posts":[]}> Data/inwards.json
-
-node KCode/ForBatV10/Backend.js
-node KCode/ForBatV10/Database.js
-call npm run home
-call npm run WA
-
-xcopy .\publicDir\WA .\public\WA /h /i /c /k /e /r /y
-
 cd ..\CrudFrontEndGulp
 call npm i
 
@@ -31,3 +21,4 @@ call npm run home
 call .\BatchFiles\Students328\V1\ForUI.bat
 
 call npm run start
+call .\BatchFiles\Students328\V1\WAUi.bat
