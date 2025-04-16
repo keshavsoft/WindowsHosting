56b21852d47715b0882b86a5f0fb4a1be5bc9d8f
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-04-16 21:51:23 +0530

    Create Pk328.bat

---
+++ BatchFiles/WhatsApp/V8/Pk328.bat
@@ -0,0 +1,32 @@
+@echo off
+
+git clone https://github.com/keshavsoft/CrudBackEndWithMailAndWA
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForClients
+git clone -c core.longpaths=true https://github.com/keshavsoft/CrudFrontEndGulp
+git clone -c core.longpaths=true https://github.com/keshavsoft/UIForWA
+
+cd CrudBackEndWithMailAndWA
+call npm i
+echo PORT=7328> .env
+echo DataPk=328>> .env
+
+cd ..\CrudFrontEndGulp
+call npm i
+
+cd ..\FrontEndForClients
+call npm i
+
+cd ..\UIForWA
+call npm i
+
+cd ..\CrudBackEndWithMailAndWA
+call .\BatchFiles\Students328\V1\forBackEndAndData.bat
+
+call .\BatchFiles\Students328\V1\forWebsite.bat
+
+call .\BatchFiles\Students328\V1\ForUI.bat
+
+call .\BatchFiles\Students328\V2\WhatsAppUi.bat
+call .\BatchFiles\Students328\V1\WithChat.bat
+
+call npm run start
