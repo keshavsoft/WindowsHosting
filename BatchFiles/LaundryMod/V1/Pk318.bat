8833ce630fdd29b4f7494f7f2538a705035fce8e
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-04-18 16:10:59 +0530

    Create Pk318.bat

---
+++ BatchFiles/LaundryMod/V1/Pk318.bat
@@ -0,0 +1,22 @@
+@echo off
+
+git clone https://github.com/keshavsoft/LaundryBEBranchV1
+git clone -c core.longpaths=true https://github.com/keshavsoft/LaundryFEBranchV1
+
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForUsers
+
+cd LaundryBEBranchV1
+call npm i
+echo PORT=7318> .env
+echo DataPk=318>> .env
+node KCode/ForBatV7/Backend.js
+call npm run Laundry
+
+cd ..\LaundryFEBranchV1
+call npm i
+
+cd ..\FrontEndForUsers
+call npm i
+
+cd ..\LaundryBEBranchV1
+call .\BatchFiles\ForLaundry\FrontEndForUsers.bat
