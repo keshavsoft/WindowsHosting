4fda31aca62ae852c7c95df966b7813064ffc037
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-04-23 11:55:14 +0530

    Create Pk328.bat

---
+++ BatchFiles/CrudBackEndWithMail/V3/Pk328.bat
@@ -0,0 +1,28 @@
+
+
+
+@echo off
+
+git clone https://github.com/keshavsoft/CrudBackEndWithMail
+git clone -c core.longpaths=true https://github.com/keshavsoft/FrontEndForClients
+git clone -c core.longpaths=true https://github.com/keshavsoft/CrudFrontEndGulp
+
+cd CrudBackEndWithMail
+call npm i
+echo PORT=7328> .env
+echo DataPk=328>> .env
+
+cd ..\CrudFrontEndGulp
+call npm i
+
+cd ..\FrontEndForClients
+call npm i
+
+cd ..\CrudBackEndWithMail
+call .\BatchFiles\Students328\V3\forBackEndAndData.bat
+
+call .\BatchFiles\Students328\V3\forWebsite.bat
+
+call .\BatchFiles\Students328\V3\ForUI.bat
+
+call npm run start
